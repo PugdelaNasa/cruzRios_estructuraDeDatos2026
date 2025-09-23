f960f9ca696385a9d77affc0a8e0e1b7ba43c7c2
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-09-22 20:27:25 -0600

    Avance Nodos Dobles

---
+++ src/n02_ListaDobleLigadas/NodoDoble.java
@@ -0,0 +1,54 @@
+package n02_ListaDobleLigadas;
+
+public class NodoDoble<T> {
+    T dato;
+    NodoDoble<T> siguiente;
+    NodoDoble<T> anterior;
+
+    public NodoDoble(T dato) {
+    }
+
+    public NodoDoble(T dato, NodoDoble<T> siguiente, NodoDoble<T> anterior) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+        this.anterior = anterior;
+    }
+
+    public NodoDoble (T dato, NodoDoble<T> siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public NodoDoble (NodoDoble<T> anterior , T dato){
+        this.dato = dato;
+        this.anterior = anterior;
+    }
+
+    public T getDato(){
+        return dato;
+    }
+
+    public void setDato(T dato){
+        this.dato = dato;
+    }
+
+    public NodoDoble<T> getSiguiente(){
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoDoble<T> siguiente){
+        this.siguiente = siguiente;
+    }
+
+    public NodoDoble<T> getAnterior(){
+        return anterior;
+    }
+
+    public void setAnterior(NodoDoble<T> anterior){
+        this.anterior = anterior;
+    }
+
+    public String toString(){
+        return "<-- "+ dato+ " -->";
+    }
+}
---
+++ src/n02_ListaDobleLigadas/listaDobleLigadas.java
@@ -0,0 +1,12 @@
+package n02_ListaDobleLigadas;
+
+public class listaDobleLigadas <T>{
+    private NodoDoble<T> head;
+    private NodoDoble<T> tail;
+    private int tamanio;
+
+    public listaDobleLigadas() {
+    }
+
+
+}
