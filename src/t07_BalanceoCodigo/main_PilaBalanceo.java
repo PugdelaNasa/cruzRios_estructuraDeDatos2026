5646fe3f83d9107942822a7a6cba78347a2537fd
Jan Carlo Cruz Rios <pug.de.la.nasa@gmail.com>
2025-10-20 18:12:22 -0600

    Verificador V2

--- src/t07_BalanceoCodigo/main_PilaBalanceo.java
+++ src/t07_BalanceoCodigo/main_PilaBalanceo.java
@@ -1,16 +1,30 @@
 package t07_BalanceoCodigo;
 
-import n07_Pilas.PilaADT;
-import n07_Pilas.Tortillas;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class main_PilaBalanceo {
     public static void main(String[] args) {
-        PilaBalanceo<Object> codigo1 = new PilaBalanceo<>();
+        PilaBalanceo<Character> codigo1 = new PilaBalanceo<>();
+        StringBuilder contenido = new StringBuilder(); // para acumular texto
+        String ruta = "src/t07_BalanceoCodigo/pruebaaa.txt" ;
 
-        String codigoFuente =" if(condicion true){hola=1}"
-                ;
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea;
 
-        System.out.println("La expresion esta balanceada?   " + codigo1.verificarBalanceo(codigoFuente));
+            while ((linea = br.readLine()) != null) {
+                contenido.append(linea).append("\n"); // lo agrega al StringBuilder
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+        String texto = contenido.toString();
+
+
+        // VERIFICAR EXPRESION
+        System.out.println("La expresion esta balanceada?   " + codigo1.verificarBalanceo(texto));
 
     }
 }
